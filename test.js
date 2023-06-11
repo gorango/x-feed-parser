84228df6e99ac103ac3f763b088f7620c2ebf7f7
Goran <gospaso@gmail.com>
2023-06-11 12:59:25 -0400

    update test, scaffold outputs

--- test.js
+++ test.js
@@ -1,27 +1,38 @@
-import fs from 'node:fs'
-import path from 'node:path'
+import { readFileSync, readdirSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
 import test from 'tape'
+import { parse } from './index.js'
 
-const root = path.join('./fixtures')
+const root = join('./fixtures')
 
-test('Fixtures', (t) => {
-	fs.readdirSync(root).forEach(async (fixture) => {
-		const inputPath = path.join(root, fixture, 'input.txt')
-		const outputPath = path.join(root, fixture, 'output.json')
-		const file = fs.readFileSync(inputPath, 'utf-8')
-		const actual = ''
+test('Fixtures', async (t) => {
+	await Promise.all(readdirSync(root).map(async (fixture) => {
+		const inputPath = join(root, fixture, readdirSync(join(root, fixture))[0])
+		const optionsPath = join(root, fixture, 'options.json')
+		const outputPath = join(root, fixture, 'output.json')
+		const file = readFileSync(inputPath, 'utf-8')
+
+		let options
+		try {
+			options = JSON.parse(readFileSync(optionsPath, 'utf-8'))
+		}
+		catch (error) {
+			options = {}
+		}
+
+		const actual = await parse(file, options)
 
 		let expected
 		try {
-			expected = JSON.parse(fs.readFileSync(outputPath, 'utf-8'))
+			expected = JSON.parse(readFileSync(outputPath, 'utf-8'))
 		}
 		catch (error) {
-			fs.writeFileSync(outputPath, `${JSON.stringify(actual, null, 2)}\n`)
+			writeFileSync(outputPath, `${JSON.stringify(actual, null, 2)}\n`)
 			return
 		}
 
 		t.deepLooseEqual(actual, expected, `should work on ${fixture}`)
-	})
+	}))
 
 	t.end()
 })
