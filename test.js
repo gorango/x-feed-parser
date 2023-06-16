42a4b4f18b4417f9c471e17f3a5f7957de9460a4
Goran <gospaso@gmail.com>
2023-06-16 09:05:11 -0400

    rename types; lint fix

--- test.js
+++ test.js
@@ -8,19 +8,10 @@ const root = join('./fixtures')
 test('Fixtures', async (t) => {
 	await Promise.all(readdirSync(root).map(async (fixture) => {
 		const inputPath = join(root, fixture, readdirSync(join(root, fixture))[0])
-		const optionsPath = join(root, fixture, 'options.json')
 		const outputPath = join(root, fixture, 'output.json')
 		const file = readFileSync(inputPath, 'utf-8')
 
-		let options
-		try {
-			options = JSON.parse(readFileSync(optionsPath, 'utf-8'))
-		}
-		catch (error) {
-			options = {}
-		}
-
-		const actual = await parse(file, options)
+		const actual = await parse(file)
 
 		// let expected
 		writeFileSync(outputPath, `${JSON.stringify(actual, null, 2)}\n`)
