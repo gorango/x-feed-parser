99b149f0844f7c7f5c34a3901ce95cca5f352213
Goran <gospaso@gmail.com>
2023-06-30 18:38:48 -0400

    chore: update types, build

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/** @typedef {import('./types.js').Feed} Feed */
+/** @typedef {import('./types.js').Item} Item */
 export { parse } from './lib/index.js'
 export { parseJsonFeed } from './lib/json.js'
 export { parseXmlFeed } from './lib/xml.js'
